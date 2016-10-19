4a583dce5329d85ac1cab8f98096697819f32a14
wk_1121 <wk_1121@163.com>
2016-10-19 14:45:41 +0800

    2016-10-19-14:44

--- js/index.js
+++ js/index.js
@@ -22,6 +22,7 @@ $(document).ready(function() {
             }
             if(index == 2){
                 if(screenWidth<=768){
+                    $(".top-top .right span").text("企业互联网化化转型");
                     $('.bottom-transparent-cover').animate({height:"75%"},divAnimationTime);
                     $('.tag-container').animate({left:"0",opacity:1},divAnimationTime);
                     $('.words-container').animate({left:"0",opacity:1},divAnimationTime);
@@ -32,6 +33,7 @@ $(document).ready(function() {
                     $('.words-container').animate({right:"0",opacity:1},divAnimationTime);
                 }
                 else{
+                    $(".top-top .right span").text("企业互联网化化转型");
                     $('.bottom-transparent-cover').animate({height:"50%"},divAnimationTime);
                     $('.tag-container').animate({left:"0",opacity:1},divAnimationTime);
                     $('.words-container').animate({left:"0",opacity:1},divAnimationTime);
@@ -40,19 +42,24 @@ $(document).ready(function() {
             }
             if(index == 3){
                 if(screenWidth<=768){
+                    $(".top-top .right span").text("全网技术平台开发");
                     $('.computer').animate({left:"0",opacity:1},divAnimationTime);
                     $('.text_t').animate({marginLeft:"0",opacity:1},divAnimationTime);
-                    $('.list_box').animate({marginRight:"0",opacity:1},divAnimationTime);
+                    $('.list_box').animate({left:"0",opacity:1},divAnimationTime);
                 }
                 else if(screenWidth>=1200){
                     var winH = $(window).height();
                     $('.pc_box').animate({height:winH+"px",opacity:1},divAnimationTime);
-                    $('.computer').animate({left:"0",opacity:1},divAnimationTime);
+                    $('.computer').animate({left:"83%",opacity:1},divAnimationTime);
                     $('.text_t').animate({left:"5%",opacity:1},divAnimationTime);
+                    $('.list_box').animate({left:"0",opacity:1},divAnimationTime);
                 }
                 else{
+                    $(".top-top .right span").text("全网技术平台开发");
                     $('.computer').animate({left:"0",opacity:1},divAnimationTime);
                     $('.text_t').animate({marginLeft:"0",opacity:1},divAnimationTime);
+                    $('.list_box').animate({left:"0",opacity:1},divAnimationTime);
+
                 }
 
             }
@@ -60,26 +67,26 @@ $(document).ready(function() {
 
                 if(screenWidth>=1200){
                     var height = $(window).height() ;
-
                     $('.list-box').animate({left:"-50%",opacity:1},divAnimationTime);
                     $('.boxright').animate({right:"0",opacity:1},divAnimationTime);
                     $('.boxleft').animate({left:"0",opacity:1},divAnimationTime);
                     $('.w-row').animate({height:height+"px",opacity:1},divAnimationTime);
                 }
                 else if(screenWidth<=768){
+                    $(".top-top .right span").text("全生态商业化运营");
                     var height = $('.section-4').height() ;
                     $('.w-container').animate({height:height+"px",opacity:1,top:"0"},divAnimationTime);
-
                 }
                 else{
+                    $(".top-top .right span").text("全生态商业化运营");
                     var height = $('.section-4').height() ;
-
                     $('.w-container').animate({height:height+"px",opacity:1,top:"0"},divAnimationTime);
 
                 }
             }
             if(index == 5){
                 if(screenWidth<=768){
+                    $(".top-top .right span").text("数字整合营销");
                     $('.bottom-transparent-cover').animate({height:"75%"},divAnimationTime);
                     $('.tag-container_2').animate({right:"0",opacity:1},divAnimationTime);
                     $('.words-container_2').animate({left:"0",opacity:1},divAnimationTime);
@@ -90,6 +97,7 @@ $(document).ready(function() {
                     $('.words-container_2').animate({left:"0",opacity:1},divAnimationTime);
                 }
                 else{
+                    $(".top-top .right span").text("数字整合营销");
                     $('.bottom-transparent-cover').animate({height:"50%"},divAnimationTime);
                     $('.tag-container_2').animate({right:"0",opacity:1},divAnimationTime);
                     $('.words-container_2').animate({left:"0",opacity:1},divAnimationTime);
@@ -98,6 +106,7 @@ $(document).ready(function() {
             }
             if(index == 6){
                 if(screenWidth<=768){
+                    $(".top-top .right span").text("品牌声誉管理");
                     $('.w-back-red').animate({height:"80%",bottom:"5%"},divAnimationTime);
                     $('.tag-container').animate({left:"0",opacity:1},divAnimationTime);
                     $('.words-container').animate({left:"0",opacity:1},divAnimationTime);
@@ -108,6 +117,7 @@ $(document).ready(function() {
                     $('.words-container').animate({right:"0",opacity:1},divAnimationTime);
                 }
                 else{
+                    $(".top-top .right span").text("品牌声誉管理");
                     $('.bottom-transparent-cover').animate({height:"50%"},divAnimationTime);
                     $('.tag-container').animate({left:"0",opacity:1},divAnimationTime);
                     $('.words-container').animate({left:"0",opacity:1},divAnimationTime);
@@ -116,6 +126,7 @@ $(document).ready(function() {
             }
             if(index == 7){
                 if(screenWidth<=768){
+                    $(".top-top .right span").text("关于我们");
                     $('.left_box ').animate({left:"0",opacity:1},divAnimationTime);
                     $('.right_box .text').animate({left:"0",opacity:1},divAnimationTime);
                     $('.right_box .xuanzhuan').animate({right:"0",opacity:1},divAnimationTime);
@@ -125,23 +136,42 @@ $(document).ready(function() {
                     $('.right_box').animate({right:"0",opacity:1},divAnimationTime);
                 }
                 else{
+                    $(".top-top .right span").text("关于我们");
                     $('.left_box ').animate({marginLeft:"0",opacity:1},divAnimationTime);
                     $('.right_box').animate({marginRight:"0",opacity:1},divAnimationTime);
                 }
 
             }
             if(index == 8){
+
+
+
                 if(screenWidth<=768){
+                    $(".top-top .right span").text("咨询");
                     $('.w-backbox').animate({height:"100%",top:"0"},divAnimationTime);
 
-                    $('.w-message-right').animate({marginLeft:"0",opacity:1,width:"100%"},divAnimationTime);
+
+
                 }
                 else if(screenWidth>=1200){
                     $('.w-backbox').animate({height:"100%",top:"0"},divAnimationTime);
-                    $('.w-message-left').animate({marginLeft:"49%",opacity:1},divAnimationTime);
-                    $('.w-message-line').animate({height:"400px",opacity:1},divAnimationTime);
-                    $('.w-message-right').animate({opacity:1},2000);
+                    $('.r1').animate({width:"80%"},1000);
+                    $('.l1').animate({marginRight:"-6%"},1000)
+                    $('.r2').animate({width:"80%"},1200);
+                    $('.l2').animate({marginLeft:"-5%"},1200)
+                    $('.r3').animate({width:"80%"},1400);
+                    $('.l3').animate({marginRight:"-6%"},1400)
+                    $('.r4').animate({width:"80%"},1600);
+                    $('.l4').animate({marginLeft:"-5%"},1600)
+                    $('.r5').animate({width:"80%"},1800);
+                    $('.l5').animate({marginRight:"-6%"},1800)
+                    $('.r6').animate({width:"80%"},2000);
+                    $('.l6').animate({marginLeft:"-5%"},2000)
+                    $('.r7').animate({width:"80%"},2200);
+                    $('.l7').animate({marginRight:"-6%"},2200)
                 }
+
+
                 else{
                     $('.w-backbox').animate({height:"100%",top:"0"},divAnimationTime);
                     $('.w-message-right').animate({marginLeft:"0",opacity:1},divAnimationTime);
@@ -150,7 +180,13 @@ $(document).ready(function() {
 
 
             }
+            if(index == 9){
+                if(screenWidth<=768){
+                    $(".top-top .right span").text("联系我们");
+                    $('.w-backbox').animate({height:"100%",top:"0"},divAnimationTime);
+                }
 
+            }
         },
 
 
@@ -179,10 +215,10 @@ $(document).ready(function() {
             }
             if(index == 3){
                 if(screenWidth>=1200){
-                    $('.pc_box').animate({height:"0",opacity:0},divAnimationTime);
-                    $('.computer').animate({left:"50px",opacity:0},divAnimationTime);
+                    $('.pc_box').animate({height:"0px",opacity:0},divAnimationTime);
+                    $('.computer').animate({left:"80%",opacity:0},divAnimationTime);
                     $('.text_t').animate({left:"-50px",opacity:0},divAnimationTime);
-
+                    $('.list_box').animate({left:"-50px",opacity:0},divAnimationTime);
                 }
                 else if(screenWidth<=768){
                     $('.text_t').animate({marginLeft:"-50px",opacity:0},divAnimationTime);
@@ -191,6 +227,7 @@ $(document).ready(function() {
                 else{
                     $('.text_t').animate({marginLeft:"-50px",opacity:0},divAnimationTime);
                     $('.computer').animate({marginRight:"-50px",opacity:0},divAnimationTime);
+                    $('.list_box').animate({left:"-50px",opacity:0},divAnimationTime);
                 }
             }
             if(index == 4){
@@ -248,16 +285,31 @@ $(document).ready(function() {
             }
             if(index == 8){
                 if(screenWidth>=1200){
+
+
                     $('.w-backbox').animate({height:"0px",opcity:0},divAnimationTime);
-                    $('.w-message-left').animate({marginLeft:"-100%",opacity:1},divAnimationTime);
-                    $('.w-message-line').animate({height:"0",opacity:1},divAnimationTime);
-                    $('.w-message-right').animate({opacity:0},divAnimationTime);
+                    $('.r1').animate({width:"0"},2200);
+                    $('.l1').animate({marginRight:"110%"},2200)
+                    $('.r2').animate({width:"0"},2000);
+                    $('.l2').animate({marginLeft:"110%"},2000)
+                    $('.r3').animate({width:"0"},1800);
+                    $('.l3').animate({marginRight:"110%"},1800)
+                    $('.r4').animate({width:"0"},1600);
+                    $('.l4').animate({marginLeft:"110%"},1600)
+                    $('.r5').animate({width:"0%"},1400);
+                    $('.l5').animate({marginRight:"110%"},1400)
+                    $('.r6').animate({width:"0"},1200);
+                    $('.l6').animate({marginLeft:"110%"},1200)
+                    $('.r7').animate({width:"0"},1000);
+                    $('.l7').animate({marginRight:"110%"},1000)
+
                 }
                 else{
                     $('.w-backbox').animate({height:"0px",opcity:0},divAnimationTime)
-                    $('.w-message-right').animate({marginLeft:"-60px",opacity:0},divAnimationTime);
+
                 }
             }
+
         }
     });
 
@@ -274,11 +326,23 @@ $(document).ready(function() {
         $(".section-3 .container .computer .img_box .img_img").eq(index).show().siblings().hide();
         $(".section-3 .container .container_text .text_tes").eq(index).show().siblings().hide();
     });
+    $(".section-4 .w-container .row .w-message .boxleft .left-row").hover(function(){
+        $(this).addClass("on").siblings().removeClass("on");
+        var index=$(this).index();
+        $(".section-4 .w-container .row .w-message .list-box img").eq(index).show().siblings().hide();
+        $(".section-4 .w-container .box-message").eq(index).show().siblings().hide();
+    });
     $(".section-5 .tag-container_2 .tag-container-tags .tag-container-tag").hover(function(){
         $(this).addClass("on").siblings().removeClass("on");
         var index=$(this).index();
         $(".section-5 .tag-container_2 img").eq(index).show().siblings().hide();
         $(".section-5 .words-container_2 .contents").eq(index).show().siblings().hide();
+    });
+    $(".section-6 .tag-container .tag-container-tags .tag-container-tag").hover(function(){
+        $(this).addClass("on").siblings().removeClass("on");
+        var index=$(this).index();
+        $(".section-6 .tag-container img").eq(index).show().siblings().hide();
+        $(".section-6 .words-container .contents").eq(index).show().siblings().hide();
     });
     $(".section-7 .right_box .xuanzhuan li").hover(function(){
         $(this).addClass("on").siblings().removeClass("on");
